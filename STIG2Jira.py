f6114a28c539dfbb43f5197b7c932fa6811b467a
J.A. Simmons V <jsimmons@houseloan.com>
2013-11-29 22:47:05 -0600

    Build basic Check, Reference, and Rule types. Need to finish TC for Group.

--- STIG2Jira.py
+++ STIG2Jira.py
@@ -16,88 +16,167 @@ Args:
 XML_FILE: file of the xml document
 """
 import xml.dom.minidom
-from xml.dom.minidom import Node
+from xml.dom.minidom import parse, parseString
+import unittest
 
-XML_FILE = U_Windows_7_V1R13_STIG_Manual-xccdf.xml #TODO jsimmons@jasimmonsv.com set this from command line
+XML_FILE = 'U_Windows_7_V1R13_STIG_Manual-xccdf.xml' #TODO jsimmons@jasimmonsv.com set this from command line
 
-class __StigCheck:
+class StigReference(object):
+    """A Reference data structure
+    
+    Attributes:
+        title: Title of the Reference
+        publisher: Who is publishing this reference
+        type: What type of reference does this correspond to.
+        subject: What subject (e.g., OS, hardware device, etc)
+        identifier: DISA provided identifier
+    """
+    title = ''
+    publisher = ''
+    type = ''
+    subject = ''
+    identifier = ''
+    
+    def __init__(self, title, pub, type, subject, id):
+        self.title = title
+        self.publisher = pub
+        self.type = type
+        self.subject = subject
+        self.identifier = id
+        
+class StigCheck(object):
     """
     This a private class is an internal class built to store the specific check data for
     a given STIG rule.
     """
-    _SYSTEM=''
-    _Name=''
-    _HREF=''
-    _CONTENT = ''
+    SYSTEM=''
+    NAME=''
+    HREF=''
+    CONTENT = ''
     def __init__(self, name, sys = '', href = '', content = ''):
         """
         This class is built to easily store and manipulate the various checks within
         a single STIG.
     
         Args:
-            system:
             name:
+            system:
             href:
             content:
     
         Raises:
         """
-        self._SYSTEM = system
-        self._NAME = name
-        self._HREF = href
-        self._CONTENT = content
+        self.SYSTEM = sys
+        self.NAME = name
+        self.HREF = href
+        self.CONTENT = content
         
-class StigRule:
+class StigRule(object):
+    """This class is built to easily store and manipulate the various rules 
+    within a single STIG.
+    
+    Attributes:
+        ID: id number of the rule
+        VERSION: DISA defined version number
+        TITLE: Summary title of the Rule
+        DESCRIPTION: Longer description of the rule
+        REFERENCE: references for the rule
+        FIXTEXT: directions on how to fix this finding when realized
+        _disa_severity: var to hold the default DISA severity
+        severity: default DISA severity, allowed to be altered
+        _disa_weight: var to hold the default DISA weight
+        weight: default DISA weight, allowed to be altered
+        checks: array of checks to perform to determine if a finding exists
+    
     """
-    This class is built to easily store and manipulate the various checks within
-    a single STIG.
+    ID = ''
+    VERSION = ''
+    TITLE = ''
+    DESCRIPTION = ''
+    REFERENCE = '' 
+    FIXTEXT = ''
+    _DISA_SEVERITY = 0
+    severity = 0
+    _DISA_WEIGHT = 0
+    weight = 0
+    checks = []
     
-    Args:
+    def __init__(self, id, ver, title, desc, ref, fixtext, severity = 0, weight=0):
+        """Inits StigGroup with id, title, desc and a null array of Rules
     
-    Raises:
-    """
-    _ID = ''
-    _VERSION = ''
-    _TITLE = ''
-    _DESCRIPTION = ''
-    _REFERENCE #TODO jsimmons@jasimmonsv.com build reference class
-    Fixtext(Fixref), fixID #TODO jsimmons@jasimmonsv.com figure out purpose of fixtext and fixID
-    _severity = 0
-    _weight = 0
-    Reference(title, publisher, type, subject, id), 
-    _check = StigCheck()
-    x=0
-    
-class StigGroup:
-    """
-    This class is built to easily store and manipulate the various checks within
-    a single STIG.
-    
-    Args:
-    
-    Raises:
-    """
-    _ID = ''
-    _TITLE=''
-    _DESCRIPTION=''
-    _RULES=[]
-    
-    def __init___(self, title, desc):
-    """
-    Initalize the Group
-    
-    Args:
-        _TITLE: The title of the STIG Group 
-        _DESCRIPTION: Description of the STIG Group
-        _RULES: Array of individual rules that belong to this STIG group
-    """
-        self._TITLE=title
-        self._DESCRIPTION=desc
-        self._RULES=[]
-    
-    def add_rule(rule):
+        Args:
+            
         """
-        Add a pre-build rule to the STIG Group
+        self.ID = id
+        self.VERSION = ver
+        self.TITLE = title
+        self.DESCRIPTION = desc
+        if isinstance(ref, StigReference):
+            self.REFERENCE = ref        
+        else: 
+            raise Exception('Expecting StigReference type for StigRule')
+        self.FIXTEXT = '' #TODO jsimmons@jasimmonsv.com figure out purpose of 
+                          #fixtext and fixID
+        self.severity = severity
+        self._DISA_SEVERITY = severity
+        self.weight = weight
+        self._DISA_WEIGHT = weight
+        
+    
+    def add_check(self, check):
+        self.checks.append(check)
+        return True
+        
+    def change_severity(self, severity):
+        self.severity = severity
+        return True
+    
+    def reset_severity(self):
+        self.severity = self._DISA_SEVERITY
+        return True
+        
+    def change_weight(self, weight):
+        self.weight = weight
+        return True
+        
+    def reset_weight(self):
+        self.weight = self._DISA_WEIGHT
+        return True
+        
+        
+class StigGroup(object):
+    """Class for Group data structure
+    
+    This class is built to easily store and manipulate the various checks within
+    a single STIG.
+    
+    Attributes:
+    ID: specific group ID
+    TITLE: Summary of the group purpose
+    DESCRIPTION: longer description of group
+    RULES: array of rules to check specific group
+    """
+    ID = ''
+    TITLE=''
+    DESCRIPTION=''
+    rules=[]
+    
+    def __init__(self, id, title, desc):
+        """Inits StigGroup with id, title, desc and a null array of Rules
+    
+        Args:
+            ID: Predefined ID of the specific STIG Group
+            TITLE: The title of the STIG Group 
+            DESCRIPTION: Description of the STIG Group
+            RULES: Array of individual rules that belong to this STIG group
+        """
+        self.ID = id
+        self.TITLE = title
+        self.DESCRIPTION = desc
+        self.rules = []
+    
+    def add_rule(self, rule):
+        """Add a pre-build rule to the STIG Group
     
         Args:
             rule: this is a prebuilt rule class
@@ -106,12 +185,9 @@ class StigGroup:
             Returns True if successful and an error if failure
         
         Raises:
-            nothing at this time
+            N/A
         """
-        Try:
-            self._RULES.append(rule)
-        Except:
-            return e
+        self.rules.append(rule)
         return True
         
     
@@ -124,7 +200,22 @@ def main():
     
     Raises:
     """
-    doc = xml.dom.minidom.parce(xml_file)
+    doc = parse(XML_FILE)
+    groups = doc.getElementsByTagName('Group')
+    for node in groups:
+        id = node.getAttribute('id')
+        for x in node.childNodes:
+            if x.nodeType == 3: #if node is a Text node
+                node.removeChild(x)
+            else:
+                if x.nodeName == 'title': 
+                    title = ''
+                elif x.nodeName == 'description':
+                    description = ''
+                elif x.nodeName == 'Rule':
+                    rules = ''
+    pass
+    print(doc)
     #TODO jsimmons@jasimmonsv.com read in xml file
     #TODO jsimmons@jasimmonsv.com parse XML FILE
     #TODO jsimmons@jasimmonsv.com move parsed files into data structure
--- TC-STIG2JIRA.py
+++ TC-STIG2JIRA.py
@@ -3,12 +3,209 @@
 Test cases for STIG2Jira app
 """
 
-import STIG2Jira
+from STIG2Jira import StigRule
+from STIG2Jira import StigGroup
+from STIG2Jira import StigCheck
+from STIG2Jira import StigReference
 import unittest
 
-class TestSequenceFunction(unittest.TestCase):
+class TestReadXML(unittest.TestCase):
     def setUp(self):
+        pass
+
+class TestCheckCheck(unittest.TestCase):
+    """All the unit test cases for the Check Class
+    """
+    test_check = None
     
+    def setUp(self):
+        sys = 'C-18095r1_chk'
+        name = 'M'
+        href = 'VMS_XCCDF_Benchmark_Windows_7_STIG.xml'
+        content = ('Analyze the system using the Security Configuration and'
+                  'Analysis snap-in. Expand the Security Configuration and'
+                  ' Analysis tree view. '
+                  ''
+                  'Navigate to Local Policies -&gt; Security Options. '
+
+'               If the value for “Audit: Shut down system immediately if unable to log security audits” is not set to “Disabled”, then this is a finding. '
+
+                'The policy referenced configures the following registry value:'
+
+                'Registry Hive: HKEY_LOCAL_MACHINE '
+                'Registry Path: \System\CurrentControlSet\Control\Lsa'
+
+                'Value Name:  CrashOnAuditFail'
+                ''
+                'Value Type:  REG_DWORD'
+                'Value:  0')
+        self.test_check = StigCheck(name, sys, href, content)
+        assertIsInstance(self.test_check, StigCheck, 'StigCheck'
+                            'was not created properly')
+        
+class TestCheckReference(unittest.TestCase):
+    """All the unit test cases for the Reference class
+    """
+    test_reference = None
+    
+    def setUp(self):
+        title = 'VMS Target Windows 7'
+        publisher = 'DISA FSO'
+        type = 'VMS Target'
+        subject = 'Windows 7'
+        id = '1712'
+        self.test_reference = StigReference(title, publisher, type, subject, id)
+        self.assertIsInstance(self.test_reference, StigReference, 'StigReference'
+                            'was not created properly')
+        
+    def test_create_reference(self):
+        title = 'VMS Target Windows 7'
+        publisher = 'DISA FSO'
+        type = 'VMS Target'
+        subject = 'Windows 7'
+        id = '1712'
+        test_reference = StigReference(title, publisher, type, subject, id)
+        self.assertIsInstance(test_reference, StigReference, 'StigReference'
+                            'was not created properly')
+class TestCheckRule(unittest.TestCase):
+    """All the unit test cases for the Rule class
+    """
+    ref = None
+    check = None
+    test_rule = None
+        
+    def setUp(self):
+        #Setup Reference type
+        title = 'VMS Target Windows 7'
+        publisher = 'DISA FSO'
+        type = 'VMS Target'
+        subject = 'Windows 7'
+        id = '1712'
+        self.ref = StigReference(title, publisher, type, subject, id)
+        self.assertIsInstance(self.ref, StigReference, 'StigReference'
+                            'was not created properly')
+        #setup Check type
+        sys = 'C-18095r1_chk'
+        name = 'M'
+        href = 'VMS_XCCDF_Benchmark_Windows_7_STIG.xml'
+        content = ('Analyze the system using the Security Configuration and'
+                  'Analysis snap-in. Expand the Security Configuration and'
+                  ' Analysis tree view. '
+                  ''
+                  'Navigate to Local Policies -&gt; Security Options. '
+
+'               If the value for “Audit: Shut down system immediately if unable to log security audits” is not set to “Disabled”, then this is a finding. '
+
+                'The policy referenced configures the following registry value:'
+
+                'Registry Hive: HKEY_LOCAL_MACHINE '
+                'Registry Path: \System\CurrentControlSet\Control\Lsa'
+
+                'Value Name:  CrashOnAuditFail'
+                ''
+                'Value Type:  REG_DWORD'
+                'Value:  0')
+        self.check = StigCheck(name, sys, href, content)
+        self.assertIsInstance(self.check, StigCheck, 'StigCheck was not created'
+                             ' properly')
+        #setup Rule Type
+        id = 'SV-25033r1_rule'
+        ver = '3.015'
+        title = 'System halts once an event log has reached its maximum size.'
+        desc = ('&lt;VulnDiscussion&gt;A system that is configured to halt if an'
+               ' event log becomes full can create a denial of service'
+               ' situation.&lt;/VulnDiscussion&gt;&lt;FalsePositives&gt;&lt;/'
+               ' FalsePositives&gt;&lt;FalseNegatives&gt;&lt;/'
+               ' FalseNegatives&gt;&lt;Documentable&gt;false&lt;/Documentable'
+               '&gt;&lt;Mitigations&gt;&lt;/Mitigations&gt;&lt;'
+               'SeverityOverrideGuidance&gt;&lt;/SeverityOverrideGuidance&gt;'
+               '&lt;PotentialImpacts&gt;&lt;/PotentialImpacts&gt;&lt;'
+               'ThirdPartyTools&gt;&lt;/ThirdPartyTools&gt;&lt;'
+               'MitigationControl&gt;&lt;/MitigationControl&gt;&lt;'
+               'Responsibility&gt;System Administrator&lt;/Responsibility&gt;'
+               '&lt;IAControls&gt;ECRR-1&lt;/IAControls&gt;')
+        fixtext = ''
+        severity = 'low'
+        weight = '10.0'
+        self.test_rule = StigRule(id, ver, title, desc, self.ref, fixtext)
+        self.assertIsInstance(self.test_rule, StigRule, 'StigRule was not created' 
+                            ' properly')
+        
+    def test_create_rule(self):
+        id = ''
+        ver = ''
+        title = ''
+        desc = ''
+        fixtext = ''
+        test_rule = StigRule(id, ver, title, desc, self.ref, fixtext)
+        self.assertIsInstance(test_rule, StigRule, 'StigRule was not created' 
+                            ' properly')
+        
+    def test_add_check(self):
+        #TODO jasimmonsv@jasimmonsv.com Build list of checks to cycle through
+        self.assertTrue(self.test_rule.add_check(self.check), 'Failed to add check to the'
+                  ' Rule')
+    
+    def test_change_severity(self):
+        pass
+        
+    def test_change_weight(self):
+        pass
+        
+    def test_reset_severity(self):
+        pass
+        
+    def test_reset_weight(self):
+        pass
+        
+class TestCheckGroup(unittest.TestCase):
+    """All unit test cases for the Group class
+    """
+    test_group = None
+    
+    def setUp(self):
+        id = 'V-1090'
+        title = 'Caching of logon credentials'
+        desc = '&lt;GroupDescription&gt;&lt;/GroupDescription&gt;'
+        rules = []
+        self.test_group = StigGroup(id, title, desc)
+        self.assertIsInstance(self.test_group, StigGroup, 'StigGroup was not created'
+                             'properly')
+    
+    def test_create_group(self):
+        id = ''
+        title = ''
+        desc = ''
+        rules = []
+        test_group = StigGroup(id, title, desc)
+        self.assertIsInstance(test_group, StigGroup, 'StigGroup was not created'
+                             'properly')
+    
+    def test_add_rule(self):
+        self.assertTrue(self.test_group.add_rule(rule))
+        
+        
+class TestCheckClass(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def test_create_check(self):
+        name = 'M'
+        sys = 'C-26718r2_chk'
+        href = 'VMS_XCCDF_Benchmark_Windows_7_STIG.xml'
+        content = ''
+        test_check = StigCheck(name, sys, href, content)
+        pass
+
+class TestRuleClass(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_create_rule(self):
+        pass
+    
+    def test_modify_rule(self):
+        pass
     
 if __name__ == '__main__':
     unittest.main()
